e72295674a4af7571aefe5ff2283c2a9b5b090dd
mrstrange47 <krayush25@gmail.com>
2024-03-22 17:00:03 +0530

    Remove unwanted lines

--- src/main/java/com/crio/coderhack/CoderhackApplication.java
+++ src/main/java/com/crio/coderhack/CoderhackApplication.java
@@ -7,16 +7,14 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-//@SpringBootApplication
+
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = UserRepository.class)
 public class CoderhackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoderhackApplication.class, args);
-		System.out.println("Working");
+		System.out.println("Server Started Successfully");
 	}
 
 }
-//mongodb+srv://krayush:<password>@atlascluster.tmvqh32.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster
-//mongodb+srv://krayush:<password>@atlascluster.tmvqh32.mongodb.net/
